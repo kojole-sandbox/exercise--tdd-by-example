afccb9cc4e35e8613ba283ba17ab52f0dc72090f
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-02-24 19:39:52 +0900

    Chapter 2

--- tests/test_money.py
+++ tests/test_money.py
@@ -6,5 +6,7 @@ from money.dollar import Dollar
 class TestMoney(unittest.TestCase):
     def test_multiplication(self):
         five = Dollar(5)
-        five.times(2)
-        self.assertEqual(five.amount, 10)
+        product = five.times(2)
+        self.assertEqual(product.amount, 10)
+        product = five.times(3)
+        self.assertEqual(product.amount, 15)
