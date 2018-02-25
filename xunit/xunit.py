34a45f3c75a4444c8f669a5ed4365a94486f1b6c
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-02-25 20:39:47 +0900

    Chapter 21

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -1,3 +1,14 @@
+class TestResult:
+    def __init__(self):
+        self.run_count = 0
+
+    def test_started(self):
+        self.run_count += 1
+
+    def summary(self):
+        return '{} run, 0 failed'.format(self.run_count)
+
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -6,10 +17,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.test_started()
         self.setup()
         method = getattr(self, self.name)
         method()
         self.teardown()
+        return result
 
     def teardown(self):
         pass
@@ -25,6 +39,9 @@ class WasRun(TestCase):
     def test_method(self):
         self.log += "test_method "
 
+    def test_broken_method(self):
+        raise Exception
+
 
 class TestCaseTest(TestCase):
     def test_template_method(self):
@@ -32,9 +49,21 @@ class TestCaseTest(TestCase):
         test.run()
         assert(test.log == "setup test_method teardown ")
 
+    def test_result(self):
+        test = WasRun('test_method')
+        result = test.run()
+        assert(result.summary() == '1 run, 0 failed')
+
+    def test_failed_result(self):
+        test = WasRun('test_broken_method')
+        result = test.run()
+        assert(result.summary() == '1 run, 1 failed')
+
 
 def main():
     TestCaseTest('test_template_method').run()
+    TestCaseTest('test_result').run()
+    # TestCaseTest('test_failed_result').run()
 
 
 if __name__ == '__main__':
