582ed7094dd02d4962c34c662ed851887fa2682a
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-02-25 16:54:37 +0900

    Chapter 19

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -2,27 +2,41 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setup(self):
+        pass
+
     def run(self):
+        self.setup()
         method = getattr(self, self.name)
         method()
 
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        super().__init__(name)
+    def setup(self):
         self.was_run = None
+        self.was_setup = 1
 
     def test_method(self):
         self.was_run = 1
 
 
 class TestCaseTest(TestCase):
+    def setup(self):
+        self.test = WasRun('test_method')
+
     def test_running(self):
-        test = WasRun('test_method')
-        assert(not test.was_run)
-        test.run()
-        assert(test.was_run)
+        self.test.run()
+        assert(self.test.was_run)
+
+    def test_setup(self):
+        self.test.run()
+        assert(self.test.was_setup)
+
+
+def main():
+    TestCaseTest('test_running').run()
+    TestCaseTest('test_setup').run()
 
 
 if __name__ == '__main__':
-    TestCaseTest('test_running').run()
+    main()
